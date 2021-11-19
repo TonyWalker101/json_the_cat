67c1cd10ba5171783dd049ba100848d33d7edc6b
Tony <awalker780@gmail.com>
2021-11-18 21:45:08 -0500

    Exported logic into a callable function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,37 @@
 const request = require("request");
-const argv = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${argv[0]}`, (error, response, body) => {
+// Main function that takes in the parameter and uses fetch to return breed info
 
+const fetchBreedDescription = breedName => {
+  return fetch(breedName);
+};
+
+// Helper function used to retrieve breed from The Cat API
+
+const fetch = breedName => {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
   
-  if (error) {
-    console.log("HTTP request error.\nUnable to locate: ", error.hostname);
-    process.exit();
-  }
+    
+    if (error) {
+      console.log("HTTP request error.\nUnable to locate: ", error.hostname);
+      process.exit();
+    }
+    
+    if (response.statusCode !== 200) {
+      console.log("Abnormal HTTP Status Code received: ", response.statusCode);
+      process.exit();
+    }
+    
+    const data = JSON.parse(body);
   
-  if (response.statusCode !== 200) {
-    console.log("Abnormal HTTP Status Code received: ", response.statusCode);
-    process.exit();
-  }
+    if (!data[0]) {
+      console.log(`The cat breed "${breedName}" could not be found in this database.`);
+      process.exit();
+    }
+    console.log(data[0].description);
   
-  const data = JSON.parse(body);
+  });
+};
 
-  if (!data[0]) {
-    console.log(`The cat breed "${argv[0]}" could not be found in this database.`);
-    process.exit();
-  }
-  console.log(data[0].description);
-
-});
+module.exports = { fetchBreedDescription };
