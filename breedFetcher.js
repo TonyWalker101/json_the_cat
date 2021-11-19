6350e86282e1e249299833a3704dbcbc39f230c2
Tony <awalker780@gmail.com>
2021-11-19 09:35:04 -0500

    Amended function to include a callback parameter

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,37 +1,33 @@
+// Function takes in a breedname and attempts to fetch data, if not it returns an error
+
 const request = require("request");
 
-// Main function that takes in the parameter and uses fetch to return breed info
-
-const fetchBreedDescription = breedName => {
-  return fetch(breedName);
-};
-
-// Helper function used to retrieve breed from The Cat API
-
-const fetch = breedName => {
+const fetchBreedDescription = (breedName, callback) => {
 
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
   
-    
     if (error) {
-      console.log("HTTP request error.\nUnable to locate: ", error.hostname);
-      process.exit();
+      const errorMessage1 = `Unable to locate "${error.hostname}"`;
+      return callback(errorMessage1, null);
     }
     
     if (response.statusCode !== 200) {
-      console.log("Abnormal HTTP Status Code received: ", response.statusCode);
-      process.exit();
+      const errorMessage2 = `Received abnormal HTTP status code "${response.statusCode}"`;
+      return callback(errorMessage2, null);
     }
     
     const data = JSON.parse(body);
   
     if (!data[0]) {
-      console.log(`The cat breed "${breedName}" could not be found in this database.`);
-      process.exit();
+      const errorMessage3 = `The cat breed "${breedName}" could not be found in this database.`;
+      return callback(errorMessage3, null);
     }
-    console.log(data[0].description);
+  
+    return callback(null, (data[0].description));
   
   });
 };
+
+
 
 module.exports = { fetchBreedDescription };
